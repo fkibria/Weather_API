9f950484facba5c636986888b4d7ddfb9d17c186
fkibria <fkibria02@gmail.com>
2020-12-26 23:04:14 -0500

    adding search UVindex function

--- script.js
+++ script.js
@@ -13,12 +13,33 @@ $(document).ready(function(){
         console.log(response.wind.speed);
         var name = response.name;
         var temp = response.main.temp;
+        temp = convertKelvin(temp);
         var humidity = response.main.humidity;
         var wind = response.wind.speed 
-
-    $("#displayWeather").append(name + "<br>" , "Temperature: " + temp + " c" + "<br>", "Humidity: " + humidity + " %" + "<br>", "Wind Speed: " + wind + " MPH");
+        var lat = response.coord.lat;
+        var long = response.coord.lon;
+        searchUVindex(lat,long);
+    $("#displayWeather").append(name + "<br>" , "Temperature: " + temp + " c" + "<br>", "Humidity: " + humidity + " %" + "<br>", "Wind Speed: " + wind + " MPH" + "<br>");
     })
    } 
+$("#searchBtn").on("click", function(){
+    var city = $("#searchBox").val()
+    searchWeather(city);
+})
 
-   searchWeather("toronto");
+
+function searchUVindex(lat, long) {
+    var apiKey = "538062e71ac8baa0d64287e376d8516e"
+    var UVQueryURL = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + long + "&appid=" + apiKey
+    $.ajax({
+        type:"GET",
+        url: UVQueryURL
+    }).then(function(response){
+        var UVIndex = response.value;
+        $("#displayWeather").append("UV Index: " + UVIndex);
+    })
+}
+let convertKelvin = (kelvin) => {
+    return parseInt(kelvin - 273.15);
+  };
 })
