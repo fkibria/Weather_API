bab75b72de7f15945f19f7169adea37721dcedda
fkibria <fkibria02@gmail.com>
2021-03-25 01:37:30 -0400

    updating some styling

--- script.js
+++ script.js
@@ -1,74 +1,94 @@
 var apiKey = "538062e71ac8baa0d64287e376d8516e"
 let months = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"];
-function setLocalStorage(key, value){
-    if(typeof value !=="object"){
+const searchHistory = document.getElementById("searchHistory");
+function showHistory() {
+    let cities = getLocalStorage("cities")
+    let myHtml = "";
+    for (let i = cities.length-1; i >= 0; i--) {
+        cityName = cities[i]
+        myHtml += `<button type="button" onclick="searchWeather('${cityName}')" class="list-group-item list-group-item-action bg-light">${cityName}</button>`
+
+        console.log(city);
+
+    }
+    searchHistory.innerHTML = myHtml;
+}
+
+$(document).ready(showHistory);
+
+function setLocalStorage(key, value) {
+    if (typeof value !== "object") {
         localStorage.setItem(key, value)
     } else {
         localStorage.setItem(key, JSON.stringify(value));
     }
 };
 
-function getLocalStorage(key){
-    try{
+function getLocalStorage(key) {
+    try {
         return JSON.parse(localStorage.getItem(key))
     }
-    catch(e) {
+    catch (e) {
         console.log(e);
         return localStorage.getItem(key);
     }
 };
 
 
-$('#searchBtn').click(function() {
+
+function removeItemOnce(arr, value) {
+    var index = arr.indexOf(value);
+    if (index > -1) {
+        arr.splice(index, 1);
+    }
+    return arr;
+}
+
+
+function searchWeather(city) {
     $(document).find('#displayWeather').css('visibility', 'inherit');
-  });
-
-
-$(document).ready(function(){
-   function searchWeather(city) {
-
-       const searchedCity = document.getElementById("searchBox");
-       const savedCityBtn = document.getElementById("searchBtn");
-       const searchedCities = document.getElementById("searchedCities");
-
-       savedCityBtn.onclick = function() {
-        let cities= getLocalStorage("cities")
-        if(cities){
-            cities.push(city)
-        } else {
-            cities = [city];
+    let cities = getLocalStorage("cities")
+    if (cities) {
+        if (cities.includes(city)) {
+            cities = removeItemOnce(cities, city)
         }
-        setLocalStorage("cities", cities);
-
-        for(let i=0; i < localStorage.length; i++) {
-            const cities = localStorage.key(i);
-            const city = localStorage.getItem(cities);
-
-            searchedCities.innerHTML += `${city}<br /> <hr>`;
-        }
- 
-       };
+        cities.push(city)
+    } else {
+        cities = [city];
+    }
+    setLocalStorage("cities", cities);
 
 
+    let myHtml = `<button onclick="searchWeather('${city}')" class="list-group-item list-group-item-action bg-light">${city}</button>`
 
-    var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q="+ city + "&appid=" + apiKey
+    console.log(city);
+
+    showHistory();
+
+
+
+
+
+
+
+    var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey
     $.ajax({
-        type:"GET",
+        type: "GET",
         url: queryUrl
-    }).then(function(response){
+    }).then(function (response) {
         var name = response.name;
         var temp = response.main.temp;
         var date = new Date();
         date = `(${date.getDate()}/${months[date.getMonth()]}/${date.getFullYear()})`;
         temp = convertKelvin(temp);
         var humidity = response.main.humidity;
-        var wind = response.wind.speed 
+        var wind = response.wind.speed
         var lat = response.coord.lat;
         var long = response.coord.lon;
         var iconcode = response.weather[0].icon;
         var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
         $('.wicon').attr('src', iconurl);
-        searchUVindex(lat,long);
+        searchUVindex(lat, long);
         getForecast(city);
         console.log(date, temp, humidity, wind, iconcode);
         $("#city").html(city);
@@ -76,33 +96,33 @@ $(document).ready(function(){
         $("#temperature").html(String(temp) + " C");
         $("#humidity").html(String(humidity) + " %");
         $("#wind-speed").html(String(wind) + " MPH");
-    // $("#displayWeather").append(name + "<br>" , "Temperature: " + temp + " c" + "<br>", "Humidity: " + humidity + " %" + "<br>", "Wind Speed: " + wind + " MPH" + "<br>");
+        // $("#displayWeather").append(name + "<br>" , "Temperature: " + temp + " c" + "<br>", "Humidity: " + humidity + " %" + "<br>", "Wind Speed: " + wind + " MPH" + "<br>");
     })
 
 
 
-   } 
+}
 
-   function getForecast(city){
-       let queryUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`
-       $.ajax({
-        type:"GET",
+function getForecast(city) {
+    let queryUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`
+    $.ajax({
+        type: "GET",
         url: queryUrl
-    }).then(function(response){
+    }).then(function (response) {
         console.log(response);
         // var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
         let { list } = response;
         let myHtml = ``;
-        for(let i=0; i < list.length;i+=8){
+        for (let i = 0; i < list.length; i += 8) {
             let { main, weather, dt_txt } = list[i];
             console.log(main, weather, dt_txt);
-            var icon = {weather};
+            var icon = { weather };
             var iconcode = icon.weather[0].icon
             console.log(icon.weather[0].icon);
             var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
-            myHtml+=`
-                <div class="card text-white bg-primary mb-3" id= "forecast" style="max-width: 200px;">
-                    <div class="card-body" style="max-width: 200px; max-height: 400px;">
+            myHtml += `
+                <div class="card text-white bg-primary mb-3" id= "forecast" style="max-width: 170px;">
+                    <div class="card-body" style="max-width: 160px; max-height: 400px;">
                         <h5 class="card-title">${dt_txt.split(" ")[0]}</h5>
                         <img src =${iconurl}>
                         <p class="card-text">Temperature: ${convertKelvin(main.temp)} C</p>
@@ -112,13 +132,13 @@ $(document).ready(function(){
             </div>
             `
         }
-    $("#five-day").html(myHtml); 
+        $("#five-day").html(`<h2 style="margin-left:5px">5-day Forecast:</h2>` + myHtml);
     })
-   }
+};
 
 
 
-$("#searchBtn").on("click", function(){
+$("#searchBtn").on("click", function () {
     var city = $("#searchBox").val()
     searchWeather(city);
 })
@@ -131,14 +151,13 @@ function searchUVindex(lat, long) {
     var apiKey = "538062e71ac8baa0d64287e376d8516e"
     var UVQueryURL = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + long + "&appid=" + apiKey
     $.ajax({
-        type:"GET",
+        type: "GET",
         url: UVQueryURL
-    }).then(function(response){
+    }).then(function (response) {
         var UVIndex = response.value;
         $("#uv").html(UVIndex);
     })
 }
 let convertKelvin = (kelvin) => {
     return parseInt(kelvin - 273.15);
-  };
-})
+};
